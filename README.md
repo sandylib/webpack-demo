3acc260abdc0a29a30edb7fdc23e36258fa09c7a
Zhang <zhangsa@tab101186.corpad.net.local>
2017-07-27 16:08:54 +1000

    asset management

---
+++ README.md
@@ -0,0 +1,1 @@
+## WebPack  3 Setup
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import _ from 'lodash';
+import './site.css';
+import me from './img/sandy.jpg';
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  var myImg = new Image();
+  myImg.src = me;
+
+  element.appendChild(myImg);
 
   return element;
 }
